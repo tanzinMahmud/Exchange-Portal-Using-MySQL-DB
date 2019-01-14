acc79fd77fc9d5991f101b39433a862ef162db43
Md. Tanzin Mahmud <tangin31@gmail.com>
2019-01-14 12:13:36 +0600

    Add files via upload

---
+++ AboutPage/AboutPage.php
@@ -0,0 +1,101 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<style>
+	body
+	{
+		margin: 0px;
+	}
+	.b_color{
+		background-color:#00bfff;
+	}
+	.oto_button{
+		background-color:#00bfff;
+		border:none;
+		color:white;
+		padding:none;
+		text-align:center;
+		text-decoration:none;
+		display:inline-block;
+		font-size:16px;
+		margin:none;
+		cursor:pointer;
+	}
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #00bfff;
+	}
+
+	li {
+		float: left;
+		border-right:none;
+	}
+
+	li:last-child {
+		border-right: none;
+	}
+
+	li a {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+	}
+
+	li a:hover:not(.active) {
+		background-color: #00bfff;
+	}
+	.nav
+	{
+		font-size:16px;
+		font-family: sans-serif;
+	}
+	.home {
+		background-color: #00bfff;
+		font-family: cursive;
+		font-size:22px;
+	}
+	div
+	{
+		position: fixed;
+		width: 100%;
+	}
+	</style>
+	</head>
+	<body>
+	<div>
+	<ul>
+  <li><a class="home" href="/ProjectV1.3/HomePage/HomePage.php">&emsp; &ensp; &ensp; &ensp; &ensp; &ensp;>> Exchangeportal&emsp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;</a></li>
+	  <li style="float:right"><a class="nav" href="/ProjectV1.3/SignUpPage/SignUpPage.php">✜Sign up</a></li>
+	  <li style="float:right"><a class="nav" href="/ProjectV1.3/SignInPage/SignInPage.php">👤Log in</a></li>
+	</ul>
+	</div>
+	</br></br></br></br></br>
+	<h3 style="color:#00bfff" align="center">About us:</h3>
+	<table align="center" style="border:3px solid #00bfff; border-radius:30px">
+	<tr>
+	<td>
+	<p style="" align="center">This is a online exchange platform. You can exchange anything in this platform. 
+	There are two ways of exchange. One is old to new exchange and another is old to old exchange. You can get a new one by giving old one and some additional cost in 
+	old to new exchange way. On the other hand you can get old one by communicating with others who wants to exchange old one with your old one.
+	</td>
+	</tr>
+	</table>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	<section align="center" style="color:#00bfff">
+	<pre>Contact us:</pre>
+	</section>
+	<address align="center">
+	Team: team_name</br>
+	House: Dhaka
+	</address>
+	</body>
+	</html>
